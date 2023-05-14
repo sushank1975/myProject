bea874da2ea16ecee324161a6bb8d2a0fa8958f3
sushank1975 <133578861+sushank1975@users.noreply.github.com>
2023-05-14 22:27:33 +0530

    Update sdi1.java

--- project1/src/project1/sdi1.java
+++ project1/src/project1/sdi1.java
@@ -5,6 +5,10 @@ public class sdi1 {
 	public static void main(String[] args) {
 
 		System.out.println("Hello world welcome java");
+		System.out.println("Hello world welcome java");
+		System.out.println("Hello world welcome java");
+		
+		
 	}
 
 }
